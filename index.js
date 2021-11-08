0b791a616e9e032f36b64fa38a6b1b48e0cc3e7d
Sarika Patel <sarikapatel1106@gmail.com>
2021-11-07 20:31:45 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var name = "Sarika Patel";
+console.log(name);
+
+var a = 2, b = 3;
+var c= a+b;
+console.log(c);
+
+var firstName ='Sarika';
+var lastName = 'Patel';
+var age = 30;
+console.log(firstName + ' ' + lastName + ' is ' +age +' years old.');
