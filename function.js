e1d6f522e3a9d00813c99b531f276b402ee84735
Sarika Patel <sarikapatel1106@gmail.com>
2021-11-07 21:16:23 -0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,27 @@
+// function
+
+console.log(changeInFahrenhit());
+
+function changeInFahrenhit()
+{
+  var cal = 32;
+  var fah = cal * 9/5 + 32;
+  return fah;
+}
+
+// nested function
+var firstName = "Sarika";
+
+console.log(middleName(firstName));
+function middleName (n)
+{
+  var mName = 'Maulik';
+  console.log(lastName(mName));
+  return n+ ' ' + lastName(mName);
+}
+
+function lastName(n)
+{
+  var surName = "Patel";
+  return n + ' ' +surName;
+}
