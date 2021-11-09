53cd88522034daffcf93320ae9c3839eb64381a4
Sarika Patel <sarikapatel1106@gmail.com>
2021-11-08 19:24:59 -0800

    Add files via upload

--- switch.js
+++ switch.js
@@ -3,6 +3,22 @@ var name = prompt("Enter your Name:");
 console.log(name);
 console.log(name.length);
 
+if (name.length < 5 && name.length != 0) {
+  console.log("name is too short");
+}
+else if (name.length > 10) {
+  console.log("name is too long");
+}
+else if (name.length == 0) {
+  console.log("name can't be empty... please enter valid name");
+}
+else {
+  console.log("your name is" + " " + name);
+}
+
+
+
+
 
 switch(name)
 {
